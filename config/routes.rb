a589e8324475699b544a44bf9adf531eebe73735
iYaipH <irimamekun@gmail.com>
2016-12-17 09:42:01 +0900

    User周りのRouterを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   resources :users
+  root :to => 'home#index'
+
+  #####
+  #user
+  #####
+  get '/mypage' => 'users#user_my_page', as: :mypage
+  get '/login' => 'users#login', as: :login
+  get '/logout' => 'users#logout', as: :logout
+  # get '/search/:word' => 'Recipes#search'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

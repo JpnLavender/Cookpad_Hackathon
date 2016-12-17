46db3d952652ab9d8c67e784d7f264df532675b7
iYaipH <irimamekun@gmail.com>
2016-12-17 17:02:18 +0900

    ReportDBModelMigrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161217071136) do
+ActiveRecord::Schema.define(version: 20161217072803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 20161217071136) do
     t.integer  "material_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "recipe_reports", force: :cascade do |t|
+    t.integer  "recipe_id"
+    t.integer  "report_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "recipes", force: :cascade do |t|

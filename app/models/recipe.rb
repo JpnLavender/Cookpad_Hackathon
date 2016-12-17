4dfdde4e167c7154b0a6ffe8d78377e3cc6ce6ea
iYaipH <irimamekun@gmail.com>
2016-12-17 15:06:12 +0900

    検索機能の追加

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,11 @@ class Recipe < ApplicationRecord
   has_many :recipe_materials
   has_many :materials, :through => :recipe_materials
   mount_uploader :image, ImageUploader
+  enum status: { men: 0, woman: 1 }
+
+  class << self
+    def search(word)
+      self.where("title like '%#{word}%'")
+    end
+  end
 end

a9aaf140b6a0b89b9947b8c79ffeb48dc40f35f2
iYaipH <irimamekun@gmail.com>
2016-12-17 09:42:53 +0900

    UserのValidationを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :uid,             uniqueness: true
+  validates :mail,            presence: true,
+    uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password_digest, presence: true
+
 end

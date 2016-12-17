ecd377a0836885a73992f20991646144004a2d6a
iYaipH <irimamekun@gmail.com>
2016-12-17 09:59:24 +0900

    ログイン・ログアウト・サインアップのrouter周りを修正

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
   #user
   #####
   get '/mypage' => 'users#user_my_page', as: :mypage
-  get '/login' => 'users#login', as: :login
-  get '/logout' => 'users#logout', as: :logout
+  get '/sign_in' => 'users#sign_in', as: :sign_in
+  get '/sign_up' => 'users#sign_up', as: :sign_up
+  get '/sign_out' => 'users#sign_out', as: :sign_out
   # get '/search/:word' => 'Recipes#search'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

bebcd2e15d8f66e20e43b2019c5cb327ad32c8ff
iYaipH <irimamekun@gmail.com>
2016-12-17 11:12:47 +0900

    recipeとmaterialの関連付け

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,4 @@
 class Recipe < ApplicationRecord
+  has_many :recipe_materials
+  has_many :materials, :through => :recipe_materials
 end
--- app/models/recipe_material.rb
+++ app/models/recipe_material.rb
@@ -1,2 +1,4 @@
 class RecipeMaterial < ApplicationRecord
+  belongs_to :recipe
+  belongs_to :material
 end

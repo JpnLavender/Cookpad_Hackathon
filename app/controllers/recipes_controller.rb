8855410c059a57521b5705da629c722be8d37b58
iYaipH <irimamekun@gmail.com>
2016-12-17 11:13:46 +0900

    add gem kaminari

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,7 +4,7 @@ class RecipesController < ApplicationController
   # GET /recipes
   # GET /recipes.json
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.page(params[:page]).per(10).order(:id)
   end
 
   # GET /recipes/1

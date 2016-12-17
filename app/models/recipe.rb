5c52fa952ca69dcaf0319529b7b5e59b20b28365
iYaipH <irimamekun@gmail.com>
2016-12-17 14:55:13 +0900

    性別のenumを定義

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,5 @@ class Recipe < ApplicationRecord
   has_many :recipe_materials
   has_many :materials, :through => :recipe_materials
   mount_uploader :image, ImageUploader
+  enum status: { men: 0, woman: 1 }
 end
